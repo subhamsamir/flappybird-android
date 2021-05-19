3ac85c037bcd11bcf8b3bc634bc78b21e8ae2ab7
Subham samir <ssrp2814@gmail.com>
2021-05-19 17:28:14 +0530

    19may

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    public float forceMagnitude;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2dReference;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+        rb2dReference = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (!isDead)
+        {
+            if (Input.GetMouseButtonDown(0)) // For the left mouse button 
+            {
+                //(PROBABLY NOT THE BEST WAY TO TAKE USER INPUT...) 
+                //AXIS ARE BETTER IN CASE USER WANTS TO CHANGE INPUT CONFIGURATIONS...
+
+
+                rb2dReference.velocity = Vector2.zero; // Shorthand writing for new Vector2(0,0). //For replicating the same behavior each time the players exerts a force (to avoid making it additive).
+                Vector2 forceVector = new Vector2(0, 1);
+                rb2dReference.AddForce(forceVector * forceMagnitude);
+
+                anim.SetTrigger("Flap");
+            }
+        }
+
+	}
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb2dReference.velocity = Vector2.zero;
+
+        isDead = true;
+        anim.SetTrigger("Die");
+
+        //Singleton pattern in action
+        GameControl.instance.BirdDied(); // the beauty of an static variable...
+    }
+
+
+
+}
---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl instance;
+
+    public GameObject gameOverText;
+    public Text scoreText;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f; // Not sure whether specifying -1.5f from the begginning actually works
+    
+
+    private int score = 0;
+
+    // Use this for initialization
+    void Awake () {
+		
+        if (instance == null)
+        {
+            // I'm needed.
+            instance = this; // this means class GameControl
+        }
+        else if (instance != this)
+        {
+            // I'm not needed.
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (gameOver)
+        {
+            if (Input.GetMouseButtonDown(0)) // For the left mouse button 
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+
+        }
+
+    }
+
+
+    public void BirdScored()
+    {
+        if (gameOver)
+            return;
+
+        score++;
+
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
